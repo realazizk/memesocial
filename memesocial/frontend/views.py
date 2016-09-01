d292f5d86d220c4c79084bb20da7c1dba7407300
mohamed-aziz <medazizknani@gmail.com>
2016-09-01 16:16:29 +0000

    [Added] frontend part, landing page

--- memesocial/frontend/views.py
+++ memesocial/frontend/views.py
@@ -1,11 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, send_from_directory, abort
+import os
 
 frontend = Blueprint(
     'frontend',
-    __name__
+    __name__,
+    template_folder='templates'
 )
+
+
+@frontend.route('/static/<dire>/<filename>')
+def serve_static(dire, filename):
+    """
+    this endpoint serves the static files in static
+    """
+    if dire not in set(['js', 'fonts', 'css', 'imgs']):
+        abort(404)
+    root_dir = os.path.dirname(os.path.realpath(__file__))
+    return send_from_directory(os.path.join(root_dir, 'static', dire), filename)
 
 
 @frontend.route('/')
 def index():
-    return 'Hello World'
+    return render_template('index.jhtml')
