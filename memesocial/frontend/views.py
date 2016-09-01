4580375df3b71712422af41cfaa6c9e48ded8803
mohamed-aziz <medazizknani@gmail.com>
2016-09-01 22:23:14 +0000

    [Edit] Work on the frontend

--- memesocial/frontend/views.py
+++ memesocial/frontend/views.py
@@ -1,11 +1,33 @@
-from flask import Blueprint, render_template, send_from_directory, abort
+from flask import Blueprint, render_template, send_from_directory, abort, session, g,\
+    jsonify
+from functools import wraps
 import os
+from itsdangerous import Serializer
+from memesocial import config
+
 
 frontend = Blueprint(
     'frontend',
     __name__,
     template_folder='templates'
 )
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if g.user is None:
+            return (jsonify({'errors': [{'detail': 'You are not allowed here sucker'}]}), 405)
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+@frontend.before_request
+def before_request():
+    g.s = Serializer(config.SECRET_KEY)
+    g.user = session.get('userData', None)
+    if g.user:
+        g.user = g.s.loads(g.user)
 
 
 @frontend.route('/static/<dire>/<filename>')
@@ -17,6 +39,11 @@ def serve_static(dire, filename):
         abort(404)
     root_dir = os.path.dirname(os.path.realpath(__file__))
     return send_from_directory(os.path.join(root_dir, 'static', dire), filename)
+
+
+@frontend.route('/dashboard')
+def dashboard():
+    return str(g.user['id'])
 
 
 @frontend.route('/')
