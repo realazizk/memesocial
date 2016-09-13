d12c7f8095f531ef35bfa91164d9ae89bd3ae53d
mohamed-aziz <medazizknani@gmail.com>
2016-09-13 15:34:29 +0100

    Create the upload folder.

--- memesocial/config.py
+++ memesocial/config.py
@@ -1,4 +1,9 @@
 from peewee import MySQLDatabase, SqliteDatabase
+import os
+
+# create images directory if it does not exist
+if not os.path.exists(os.path.join(os.getcwd(), 'images')):
+    os.makedirs(os.path.join(os.getcwd(), 'images'))
 
 
 # my base config which is a shared class
@@ -6,7 +11,8 @@ class baseConfig(object):
     SECRET_KEY = "myTopSecretKey"
     PLUS_CLIENT_ID = '277809981459-v1buepo73pneahp7p7gmrdbkfhmi5m4g.apps.googleusercontent.com'
     PLUS_CLIENT_SECRET = 'K_P9lEWiLSBiAn6_gczl48Vf'
-    UPLOAD_FOLDER = '/home/mohamed/px1/images/'
+    UPLOAD_FOLDER = os.path.join(os.getcwd(), 'images')
+    
     ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
     SITE_URL = 'http://localhost:1337'
     PROFILE = True
