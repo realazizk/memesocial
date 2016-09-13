1b7663d0b06bac04b31d2f59b627d664390ef561
mohamed-aziz <medazizknani@gmail.com>
2016-09-13 14:49:09 +0100

    Fixed the tests.

--- memesocial/tests/test_api.py
+++ memesocial/tests/test_api.py
@@ -10,7 +10,7 @@ def client(request):
     import memesocial
     memesocial.init('memesocial.config.testingConfig')
     client = memesocial.app.test_client()
-    
+
     with memesocial.app.app_context():
         try:
             memesocial.db.drop_tables(memesocial.all_tables)
@@ -18,7 +18,7 @@ def client(request):
             # This occurs when the table does not exist
             pass
         memesocial.db.create_tables(memesocial.all_tables)
-    
+
     return client
 
 
@@ -33,8 +33,7 @@ def register_user(client, username, password):
             'password': password,
             'email': username + '@nobody.com'
         }),
-        content_type='application/json'
-    )
+        content_type='application/json')
     assert response.status_code == 200
 
 
@@ -53,8 +52,7 @@ def do_login(client, username='mohamed', password='123'):
             'username': username,
             'password': password
         }),
-        content_type='application/json'
-    )
+        content_type='application/json')
     return response.status_code
 
 
@@ -65,31 +63,27 @@ def do_logout(client):
 def update_profile_image(client, imageName):
     response = client.post(
         '/api/update_profile_image',
-        data=open(imageName, 'r').read(),
-        content_type='application/octet-stream'
-    )
+        content_type='multipart/form-data',
+        buffered=True,
+        data={
+            'file-input': (open(imageName, 'rb'), imageName)
+        })
     return response.status_code
 
 
-def update_cover(client, imageName):
-    response = client.post(
-        '/api/update_cover_image',
-        data=open(imageName, 'r').read(),
-        content_type='application/octet-stream'
-    )
+def update_cover(client, colorCode):
+    response = client.get('/api/update_cover/%s' % colorCode)
     return response.status_code
 
 
 def mk_content(client, imageName, description):
-    import base64
     response = client.post(
         '/api/create_content',
-        data=json.dumps({
-            'desc': description,
-            'image': base64.b64encode(open(imageName, 'r').read())
-        }),
-        content_type='application/json',
-    )
+        content_type='multipart/form-data',
+        buffered=True,
+        data={
+            'input-21': (open(imageName, 'rb'), imageName)
+        })
     return (response.status_code, json.loads(response.data))
 
 
@@ -110,28 +104,23 @@ def test_auth_system(client):
 def test_change_images(client):
     register_user(client, 'mohamed', '123')
     do_login(client)
-    assert update_cover_image(client, 'imgTest.jpg') == 200
+    assert update_cover(client, 'EC407A') == 200
+    assert update_cover(client, 'FFFFFF') == 422
     assert update_profile_image(client, 'imgTest.jpg') == 200
 
 
 def heart_content(client, cid):
-    response = client.get(
-        '/api/heartit/%i' % cid,
-    )
+    response = client.get('/api/heartit/%i' % cid, )
     return response.status_code
 
 
 def unheart_content(client, cid):
-    response = client.get(
-        '/api/unheartit/%i' % cid
-    )
+    response = client.get('/api/unheartit/%i' % cid)
     return response.status_code
 
 
 def get_content(client, cid):
-    response = client.get(
-        '/api/content/%i' % cid
-    )
+    response = client.get('/api/content/%i' % cid)
     return response.status_code, json.loads(response.data)
 
 
@@ -162,7 +151,8 @@ def test_content(client):
     r = get_content(client, cid)
     assert r[0] == 200
     assert 'success' in r[1]
-    assert len(r[1]['success'][0]['hearters']) == 2
+    print r
+    assert len(r[1]['success']['hearters']) == 2
 
     # test unheart
     assert unheart_content(client, cid) == 200
@@ -173,7 +163,7 @@ def test_content(client):
     r = get_content(client, cid)
     assert r[0] == 200
     assert 'success' in r[1]
-    assert len(r[1]['success'][0]['hearters']) == 1
+    assert len(r[1]['success']['hearters']) == 1
 
 
 def do_follow(client, user, password, target):
@@ -189,8 +179,7 @@ def do_comment(client, contentId, commentContent):
             'content_id': contentId,
             'comment_content': commentContent
         }),
-        content_type='application/json'
-    )
+        content_type='application/json')
     return response.status_code
 
 
@@ -210,7 +199,7 @@ def test_comment(client):
     assert do_comment(client, 1, 'Really funny lol') == 200
     rv = get_content(client, 1)
     assert rv[0] == 200
-    assert len(rv[1]['success'][0]['commentors']) == 1
+    assert len(rv[1]['success']['commentors']) == 1
 
     # test of comment on content that does not exist
     assert do_comment(client, 97, 'Fuck all you') == 404
@@ -260,13 +249,19 @@ def test_network(client):
     do_login(client, username='mohamed', password='123')
 
     # my tests for now
-    assert json.loads(client.get('/api/maybe_like').data) == {u'4': 0.12254901960784313, u'5': 0.1830065359477124, u'6': 0.14215686274509803}
+    assert json.loads(client.get('/api/maybe_like').data) == [
+        {u'username': u'unke1',
+         u'image_profile': u'/images/anonymous.png',
+         u'id': 4}, {u'username': u'test1',
+                     u'image_profile': u'/images/anonymous.png',
+                     u'id': 5}, {u'username': u'gigi1',
+                                 u'image_profile': u'/images/anonymous.png',
+                                 u'id': 6}
+    ]
 
 
 def get_feed(client):
-    response = client.get(
-        '/api/news_feed'
-    )
+    response = client.get('/api/news_feed')
     return response.data
 
 
@@ -288,7 +283,8 @@ def test_meme_feed(client):
     do_content(client, 'unke1', 'unke1', 'imgTest.jpg', 'My description')
     do_heart(client, 'gauss', 'motherfucker', 2)
 
-    do_content(client, 'gauss', 'motherfucker', 'imgTest.jpg', 'My description')
+    do_content(client, 'gauss', 'motherfucker', 'imgTest.jpg',
+               'My description')
     do_heart(client, 'unke1', 'unke1', 3)
 
     do_follow(client, 'gauss', 'motherfucker', 1)
@@ -300,7 +296,7 @@ def test_meme_feed(client):
     do_follow(client, 'euler', 'mathisCool', 2)
     do_follow(client, 'euler', 'mathisCool', 1)
     do_follow(client, 'gauss', 'motherfucker', 3)
-    
+
     do_follow(client, 'gigi1', 'pipi1', 1)
     do_follow(client, 'gigi1', 'pipi1', 3)
     do_follow(client, 'gigi1', 'pipi1', 5)
